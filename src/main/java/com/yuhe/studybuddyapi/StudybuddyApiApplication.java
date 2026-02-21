519d07a50c25a7cafc1a73ed0ea7933a2f966f71
yuhezhang1014 <yuhezhang74@gmail.com>
2026-02-22 04:13:14 +0800

    chore: bootstrap spring boot project

---
+++ src/main/java/com/yuhe/studybuddyapi/StudybuddyApiApplication.java
@@ -0,0 +1,13 @@
+package com.yuhe.studybuddyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudybuddyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudybuddyApiApplication.class, args);
+    }
+
+}
